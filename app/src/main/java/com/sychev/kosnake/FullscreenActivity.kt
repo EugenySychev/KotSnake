e86382960e7ff587a11e1b56b9eff39785a28912
Eugeny Sychev <eugeny.sychev@gmail.com>
2021-04-20 18:48:10 +0300

    Initial add one more activity

--- app/src/main/java/com/sychev/kosnake/FullscreenActivity.kt
+++ app/src/main/java/com/sychev/kosnake/FullscreenActivity.kt
@@ -1,23 +1,17 @@
 package com.sychev.kosnake
 
-import android.graphics.Color
-import android.graphics.Paint
-import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+
 class FullscreenActivity : AppCompatActivity(), MenuDrawer.SelectedItem {
 
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var menu = MenuDrawer(this)
+
         val list = listOf<String>("Main", "Second", "Third")
         menu.setSelectedItemHandler(this)
         menu.setItemArray(list)
---
+++ app/src/main/java/com/sychev/kosnake/SnakeGameActivity.kt
@@ -0,0 +1,12 @@
+package com.sychev.kosnake
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SnakeGameActivity : AppCompatActivity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
